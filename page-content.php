a8f99292e910973bcc0f3e349377e2cc92ce9498
Filip Swinarski <filip.swinarski@gmail.com>
2016-01-16 12:18:39 +0100

    applied for wordpress child theme

---
+++ page-content.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template used for displaying page content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen Contratiempo Child
+ * @since Twenty Fifteen Contratiempo Child 0.1.0
+ * 16.01.2016 v. 0.1.0 @filip-swinarski
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+		// Post thumbnail.
+		twentyfifteen_post_thumbnail();
+	?>
+
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+        <?php 
+            $server=$_SERVER['REQUEST_URI'];
+            if ($server == '/about/') {
+                include 'elements/band-members.html';
+            }
+        ?>
+	<?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer><!-- .entry-footer -->' ); ?>
+
+</article><!-- #post-## -->

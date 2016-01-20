1af03f57ed129cf4f3dc24c1f1dff998b9ba3666
Filip Swinarski <filip.swinarski@gmail.com>
2016-01-20 01:09:43 +0100

    added custom js scripts and header

---
+++ scripts.js
@@ -0,0 +1,17 @@
+/* Custom js application for Contratiempo
+ * namespace: CONTRA,
+ * v. 0.1.0 19.01.2016 @filip-swinarski
+ */
+
+var CONTRA = function() {
+ 
+   function test() {
+       console.log(1);
+   }
+
+   test();
+};
+
+window.addEventListener('load', function() {
+    CONTRA();
+})

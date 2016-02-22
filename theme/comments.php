84e2a939328e557af0caf456d54a8dd577756e4e
Filip Swinarski <filip.swinarski@gmail.com>
2016-02-22 18:01:38 +0100

    Disabled showing comments

---
+++ theme/comments.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying comments
+ *
+ * The area of the page that contains both current comments
+ * and the comment form.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
+?>
+
+<div id="comments" class="comments-area">
+	<?php if ( have_comments() ) : ?>
+
+
+	<?php endif; // have_comments() ?>
+
+	<?php
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+	<?php endif; ?>
+
+</div><!-- .comments-area -->

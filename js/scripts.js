4ed92f57b96e3ea3c7ab6cd9a936cbe09e04099f
Filip Swinarski <filip.swinarski@gmail.com>
2016-02-25 15:08:56 +0100

    js animations on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,72 @@ var CONTRA = function() {
     function test() {
         console.log(0);
     }
-    
-    // views
 
+    var headings = Array.prototype.slice.call(document.querySelectorAll('.entry-title, .entry-title a')),
+        logo = document.querySelector('.site-logo'),
+            banners = Array.prototype.slice.call(document.querySelectorAll('.post-thumbnail')),
+            containers = Array.prototype.slice.call(document.querySelectorAll('.entry-content')),
+        footers = Array.prototype.slice.call(document.querySelectorAll('.entry-footer *')),
+        postNav = Array.prototype.slice.call(document.querySelectorAll('.post-navigation a *')),
+        postNavBtns = Array.prototype.slice.call(document.querySelectorAll('.post-navigation .nav-next, .post-navigation .nav-previous')),
+        bandMembers = document.querySelector('.band-members'),
+        siteFooter = document.querySelector('.site-footer .site-info');
+
+    function animateScroll(el, dist, className, direction) {
+        if (!el) return;
+        var winOffset = window.scrollY,
+            winHeight = window.innerHeight,
+            elOffset = parseInt(el.getBoundingClientRect().top) + winOffset,
+            elHeight = el.offsetHeight;
+        if (elOffset > winOffset + dist && elOffset + elHeight + dist < winOffset + winHeight) {
+            if (direction === 'in') 
+                el.classList.add(className);
+            else 
+                el.classList.remove(className);
+        } else {
+            if (direction === 'in') 
+                el.classList.remove(className);
+            else 
+                el.classList.add(className);
+        }
+    }
+
+    function addListeners() {
+        window.addEventListener('scroll', function(e) {
+            var timeout;
+            headings.forEach(function(el) {
+                if (el.parentNode.parentNode.querySelector('.post-thumbnail'))
+                        animateScroll(el, 150, 'active', 'in');
+                else
+                    animateScroll(el, 50, 'active', 'in');
+                animateScroll(el, 0, 'darkened', 'out');
+                animateScroll(el, -10, 'lightened', 'in');
+            });
+            animateScroll(logo, 50, 'coloured', 'out');
+            banners.forEach(function(el) {
+                animateScroll(el, -parseInt(el.clientHeight) + 100, 'darkened', 'out');
+            });
+            containers.forEach(function(el) {
+                animateScroll(el, -parseInt(el.clientHeight) + 100, 'darkened', 'out');
+            });
+            footers.forEach(function(el) {
+                animateScroll(el, 20, 'darkened', 'out');
+            });
+            postNav.forEach(function(el) {
+                animateScroll(el, 50, 'darkened', 'out');
+            });
+            postNavBtns.forEach(function(el) {
+                animateScroll(el, -80, 'darkened', 'out');
+            });
+            if (bandMembers) animateScroll(bandMembers, -parseInt(bandMembers.clientHeight) + 100, 'darkened', 'out');
+            animateScroll(siteFooter, -1, 'darkened', 'out')
+        });
+    }
+    
     // init
     function init() {
         
+        addListeners();
         test();
         
     }
